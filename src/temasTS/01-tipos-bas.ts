978ca2eae9101fdd8afe3cbdf83fe8347d258734
IEVN1003-22001388 <fernando.gp9898@gmail.com>
2025-09-05 18:56:37 -0600

    Segundo Commit

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,18 @@
+console.log("hola mundo")
+
+var num1 =23;
+let num2=32;
+const num3=67;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+let sum =num1+num2+num3;
+console.log("la suma es: "+sum);
+
+let matricula:number|string;
+
+matricula=2200138
+matricula='SHV7789'
+console.log(matricula)
